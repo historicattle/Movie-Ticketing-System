c7ae67fbd130e34a04de4e6327b8f756298c00e1
ROHINTH S <imt_2024068@iiitm.ac.in>
2025-07-19 04:02:30 +0530

    Add routes for showtime and booking endpoints in server.Add routes for showtime and booking endpoints in server.Update index.js

--- backend/server/index.js
+++ backend/server/index.js
@@ -2,20 +2,25 @@ import express from "express";
 import mongoose from "mongoose";
 import authRoutes from "./routes/authRoutes.js";
 import movieRoutes from "./routes/movieRoutes.js";
+import showtimeRoutes from "./routes/showtimeRoutes";
+import bookingRoutes from "./routes/bookingRoutes";
 
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/movies", movieRoutes);
+app.use("/api/showtimes", showtimeRoutes);
+app.use("/api/bookings", bookingRoutes);
 
-mongoose.connect("mongodb://localhost:27017/movieapp", {//temporary
+mongoose.connect("mongodb://localhost:27017/movieapp", {
+//temporary
   useNewUrlParser: true,
   useUnifiedTopology: true
-}).then(() => console.log("MongoDB connected"))
+})
+.then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
